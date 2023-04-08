00819e26a6bf428061b0a43c1561c15e5aafeb0a
Asalim9 <70577685+Asalim9@users.noreply.github.com>
2023-04-08 14:22:42 +0300

    Update app.js

--- mod/js/app.js
+++ mod/js/app.js
@@ -1,8 +1,11 @@
 // replace these values with those generated in your TokBox Account
-var apiKey = "47241704";
+//var apiKey = "47241704";
+//var sessionId = "1_MX40NzI0MTcwNH5-MTYyMjIwNjkzMzI5N352amcrckZWbzZTLzM3cmRqVmxaQ1NodXh-fg";
+//var token = "6c8b1deb3f04a41544a69466c29ea8b9916e6ea3";
 
-var sessionId = "1_MX40NzI0MTcwNH5-MTYyMjIwNjkzMzI5N352amcrckZWbzZTLzM3cmRqVmxaQ1NodXh-fg";
-var token = "6c8b1deb3f04a41544a69466c29ea8b9916e6ea3";
+var apiKey = "47696341";
+var sessionId = "2_MX40NzY5NjM0MX5-MTY4MDk1MjM1MjIwNX5CdFFZQzE2dXE4Rk55ME92WjVhRmV4WUZ-fn4";
+var token = "var token = "T1==cGFydG5lcl9pZD00NzY5NjM0MSZzaWc9N2UzN2M2NzMxMWU4MmQxNTIzOThiZGMzYmFhY2ZjNTcwNzA2M2NlNjpzZXNzaW9uX2lkPTJfTVg0ME56WTVOak0wTVg1LU1UWTRNRGsxTWpNMU1qSXdOWDVDZEZGWlF6RTJkWEU0Ums1NU1FOTJXalZoUm1WNFdVWi1mbjQmY3JlYXRlX3RpbWU9MTY4MDk1MjQxMCZub25jZT0wLjgxNzY5ODI4NTMwNDM0NDYmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTY4MzU0NDQwOSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
 
 //var apiKey = "4102714";
 //var token = "c4bc9a8e9fb12536b60b92252c98637dc026e921"
